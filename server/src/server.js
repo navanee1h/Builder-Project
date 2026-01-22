c000e83cb64e1bbb69ec70d1bae8ac06f7752d25
Navaneeth <117712420+navanee1h@users.noreply.github.com>
2026-01-22 11:38:43 +0530

    update cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import app from './app.js';
 import connectDB from './config/db.js';
 import { config } from './config/env.js';
+import cors from "cors";
 
 // Connect to Database
 connectDB();
@@ -10,3 +11,10 @@ const PORT = config.port;
 app.listen(PORT, () => {
     console.log(`Server running in ${config.nodeEnv} mode on port ${PORT}`);
 });
+
+app.use(cors({
+    origin: [
+        "https://builders-fhx3.onrender.com",
+        "https://builder-admin.onrender.com"
+    ]
+}));
